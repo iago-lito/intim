d8571d25e31b189b8891c2751bf6d2002504d00a
Iago-lito <iago.bonnici@gmail.com>
2017-03-11 17:52:12 +0100

    Analyse the current session to type nodes
    - Prepare "Type" objects to add relevant information to the nodes, *i.e.* type
      the forest
        - link them to actual python types with `types_map`
    - add backward iteration procedure to get full `path` to a node
    - source the whole file as a test
    - analyse node types with `eval` within `globals()`

--- plugin/lexing.py
+++ plugin/lexing.py
@@ -8,38 +8,67 @@ script!
 
 from pygments.token import Token, is_token_subtype
 from pygments.lexers import python as pylex
+from enum import Enum # for analysing enum types
+import os # for module type
 
-source = \
-"""
-a = 5
-b = a + 1
-b.plainMember = 'wops'
-b.plainMethod(arguments)
-b.plainMethod(arguments, andeven, more=arguments[say.even, complex])
-b.met.a.long.navigation()
-b. met
-    .
-    a
-    .weird . navigation
-then.a.subsequent.one
-"charstring"
-\"\"\"and a
-multilined
-one with a \" trap
-\"\"\"
-def wops(irregular)
-    code.. still tokenized? yeah!
-"""
+# Use this very file as a test :P
+
+with open('./lexing.py', 'r') as file:
+    source = file.read()
+
+class Type(object):
+    """Type class for typing nodes of the token forest
+    Can iterate over its instances for convenience
+    """
+
+    _instances = set()
+
+    def __init__(self, id, color, python_type):
+        self.id = id
+        self._instances.add(self)
+        self.color = color
+        self.type = python_type
+
+    @classmethod
+    def instances(cls):
+        """Iterate over all instances
+        """
+        return iter(cls._instances)
+
+Instance   = Type("instance"   , 'blue'   , None)
+Unexistent = Type("unexistent" , 'grey'   , None)
+Class      = Type("class"      , 'gold'   , type(object))
+Function   = Type("function"   , 'purple' , type(lambda a: a))
+BuiltIn    = Type("builtin"    , 'orange' , type(dir))
+Module     = Type("module"     , 'pink'   , type(os))
+Int        = Type("int"        , 'green'  , type(1))
+Float      = Type("float"      , 'green'  , type(1.))
+String     = Type("string"     , 'green'  , type('a'))
+Bool       = Type("bool"       , 'green'  , type(True))
+NoneType   = Type("nonetype"   , 'grey'   , type(None))
+
+# Store them so that they can easily be found from actual python types
+types_map = {}
+for cls in Type.instances():
+    types_map[cls.type] = cls
+
 
 class Node(object):
     """Identifier and references to its parents and kids. It may have no
     parent, it is a root then.
     """
 
-    def __init__(self, id, parent=None):
+    def __init__(self, id, parent=None, type=Unexistent):
+        """
+        id: string the node's identifier: i.e. how it is written in the
+            code.
+        parent: Node its parent node in the graph, root node if None
+        type: Type associated type with coloration etc
+        """
         self.id = id
         self.parent = parent
         self._kids = {} # {id: Node}
+        self.type = type
 
     @property
     def leaf(self):
@@ -66,11 +95,27 @@ class Node(object):
         return node
 
     @property
+    def parents(self):
+        """iterate backwards until a root parent is found
+        """
+        yield self
+        if self.parent:
+            yield from self.parent.parents
+        else:
+            raise StopIteration()
+
+    @property
+    def path(self):
+        """Use backward iteration to build the full path to this node
+        """
+        res = [parent.id for parent in self.parents]
+        return '.'.join(reversed(res))
+
+    @property
     def kids(self):
         """iterate over kids
         """
-        for kid in self._kids.values():
-            yield kid
+        return iter(self._kids.values())
 
     @property
     def leaves(self):
@@ -92,13 +137,45 @@ class Node(object):
     def _repr(self, prefix):
         """Iterate over all nodes and print full paths
         """
-        res = prefix + self.id + '\n'
+        res = "{}{}: {}\n".format(prefix, self.id, self.type.id)
         for kid in self.kids:
             res += kid._repr(prefix + self.id + '.')
         return res
 
     def __repr__(self):
         return self._repr('')
+
+    def __len__(self):
+        """Number of nodes: ourselves as a node + the weight of our kids
+        """
+        return 1 + sum(len(kid) for kid in self.kids)
+
+    def type_nodes(self, prefix=''):
+        """Ultimate use of this forest: evaluate our id in this context
+        to retrieve information on the current state of this access path
+        prefix: string previous path (context) of this node
+        called by the parents
+        """
+        path = prefix + self.id
+        # analyse type of this node:
+        try:
+            t = eval("type({})".format(path), globals())
+        except (AttributeError, NameError) as e:
+            # then all subsequent nodes are unexistent
+            for node in self:
+                node.type = Unexistent
+            return
+        # is the type available, special?
+        node_type = types_map.get(t)
+        if node_type:
+            self.type = node_type
+        else:
+            # then it is just a plain valid, known node, probably
+            # instance of a custom class
+            self.type = Instance
+        for kid in self.kids:
+            kid.type_nodes(path + '.')
+
 
 class Forest(Node):
     """A Forest is a special Node with no parent, no id, and containing
@@ -108,6 +185,12 @@ class Forest(Node):
     def __init__(self):
         self._kids = {}
 
+    @property
+    def parents(self):
+        """A forest has no parents
+        """
+        raise StopIteration()
+
     def __repr__(self):
         if self.leaf:
             return "empty Forest."
@@ -116,13 +199,31 @@ class Forest(Node):
             res += repr(kid)
         return res
 
+    def __len__(self):
+        """Total number of nodes in the forest:
+        """
+        return sum(len(kid) for kid in self.kids)
+
+    def __iter__(self):
+        """Iterate over all trees, not ourselves
+        """
+        for kid in self.kids:
+            yield from kid
+
+    def type_nodes(self):
+        """Ask each tree to type itself
+        """
+        for kid in self.kids:
+            kid.type_nodes()
+
+
 # informal tests:
 a = Node('abc')
 d = Node('de')
 f = Node('f')
 a.add_node(d)
 a.add_node(f)
-f.add_id('gh')
+gh = f.add_id('gh')
 f.add_id('i')
 forest = Forest()
 forest.add_node(a)
@@ -132,33 +233,46 @@ A.add_id('C')
 forest.add_node(A)
 forest.add_id('B')
 print(forest)
+gh.path
 
 # dear lexer
 lx = pylex.Python3Lexer()
 g = lx.get_tokens(source)
 # gather names to color as a forest of '.' operators:
-# iterate over type_of_token, string
 forest = Forest()
 current = forest
 # flag to keep track of whether to add in depth or go back to the root
 last_was_a_name = True
-# Also gather misc:
-keywords = set()
-litterals = set()
-operators = set()
+# Also gather misc immediate tokens.. just for fun and extensibility
+misc = {}
+for i in [Token.Name.Decorator,
+          Token.Name.Namespace,
+          Token.Name.Operator,
+          Token.Name.Keyword,
+          Token.Name.Literal,
+          Token.Comment,
+          ]:
+    misc[i] = set()
+# iterate over type_of_token, string
 for t, i in g:
+    in_misc = False
+    for subtype, harvest in misc.items():
+        if is_token_subtype(t, subtype):
+            in_misc = True
+            harvest.add(i)
+            break
+    if in_misc:
+        # no need to go further since this token does not belong to the forest
+        continue
     if is_token_subtype(t, Token.Name):
+        if i == '.':
+            raise Exception()
         node = forest if last_was_a_name else current
         current = node.add_id(i)
         last_was_a_name = True
-    if is_token_subtype(t, Token.Operator):
-        operators.add(i)
+    elif is_token_subtype(t, Token.Operator):
         if i == '.':
             last_was_a_name = False
-    if is_token_subtype(t, Token.Keyword):
-        keywords.add(i)
-    if is_token_subtype(t, Token.Literal):
-        litterals.add(i)
-print(forest)
 
+forest.type_nodes()
 
