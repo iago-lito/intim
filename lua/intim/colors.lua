9fb62c51fd45f8037abf77abcc1226e0e9f78895
Iago-lito <iago-lito@cleb.io>
2026-08-17 17:06:05 +0200

    🌱 Collect lua identifiers and paths.

--- lua/intim/colors.lua
+++ lua/intim/colors.lua
@@ -1,17 +1,81 @@
 local M = {}
 local str = require("intim.strings")
 local err = require("intim.errors")
+local I = require("intim.state")
 
-local lang = "lua"
+---@alias Query vim.treesitter.Query
 
--- Implement using this.. weird trick?
+--------------------------------------------------------------------------------
+--- Retrieve (root) identifiers and paths like `a.b.c`.
+
+---@alias Paths table<string, boolean> Store as unique "a.b.c" chains.
+---@alias Identifiers table<string, boolean> The `a` in the above, or just `d`.
+
+local queries = {
+  ids = [[;query
+    ((identifier) @id
+    (#not-has-parent? @id dot_index_expression))
+  ]],
+  paths = [[;query
+    ((dot_index_expression) @path
+      (#not-has-parent? @path dot_index_expression))
+  ]],
+}
+for k, p in pairs(queries) do
+  ---@cast queries table<string, Query>
+  queries[k] = vim.treesitter.query.parse("lua", p)
+end
+
+---@return Identifiers, Paths
+function M.collect()
+  local tree = vim.treesitter.get_parser():parse()[1]:root()
+  ---@type fun(node:TSNode):string
+  local function text(node) return vim.treesitter.get_node_text(node, 0, {}) end
+  local roots = {} ---@type Identifiers
+  local paths = {} ---@type Paths
+  for _, node, _, _ in queries.ids:iter_captures(tree, 0) do
+    local name = text(node)
+    roots[name] = true
+  end
+  for _, node, _, _ in queries.paths:iter_captures(tree, 0) do
+    -- Check path for identifier-purity:
+    -- chains like a.b.c.method_call().d.e are truncated.
+    -- The `a.b.c` path should have been independently captured.
+    local full = text(node)
+    local impure = false
+    local root ---@type string
+    while true do
+      if node:type() == "identifier" then
+        root = text(node)
+        break
+      end
+      if node:type() ~= "dot_index_expression" then
+        impure = true
+        break
+      end
+      local child = node:child(0) ---@cast child -?
+      node = child
+    end
+    if not impure then
+      roots[root] = true
+      paths[full] = true
+    end
+  end
+  vim.print("roots: " .. vim.inspect(roots))
+  vim.print("paths: " .. vim.inspect(paths))
+  return roots, paths
+end
+
+--------------------------------------------------------------------------------
+-- Color using this.. weird trick?
 -- https://github.com/neovim/neovim/issues/41354
+
 local intim_patterns ---@type integer[]
 local toggle_counter = 0
 
----@alias Query vim.treesitter.Query
 ---@type fun(_: lang):Query?
-local function get_query(lang)
+local function get_query()
+  local lang = I.current_lang()
   local q = vim.treesitter.query.get(lang, "highlights")
   if not q then
     err.err("No query file found for " .. vim.inspect(lang) .. ".")
@@ -20,6 +84,7 @@ local function get_query(lang)
 end
 
 local function start_coloring()
+  local lang = I.current_lang()
   local current = get_query(lang)
   if not current then return end
   local pats = current.info.patterns
@@ -53,6 +118,7 @@ local function start_coloring()
 end
 
 local function end_coloring()
+  local lang = I.current_lang()
   local q = get_query(lang)
   if not q then return end
   for _, pat in ipairs(intim_patterns) do
@@ -63,6 +129,7 @@ local function end_coloring()
 end
 
 -- DEBUG.
+vim.keymap.set({ "n" }, "UP", M.collect)
 vim.keymap.set({ "n" }, "TT", start_coloring)
 vim.keymap.set({ "n" }, "tt", end_coloring)
 
