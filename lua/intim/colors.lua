1546f5bc8310f88bddadc8465f9c8afec494d1b7
Iago-lito <iago-lito@cleb.io>
2026-08-17 12:48:59 +0200

    🌱 Alternate primitives with 'highlights' query.

--- lua/intim/colors.lua
+++ lua/intim/colors.lua
@@ -1,66 +1,65 @@
 local M = {}
+local str = require("intim.strings")
 local err = require("intim.errors")
 
--- Investigate tree-sitter query+highlight?
-
 local lang = "lua"
-local ns = vim.api.nvim_create_namespace("intim")
-local function clear_all(buf) vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1) end
 
-local q = vim.treesitter.query.parse(
-  lang,
-  [[;; extends
-    ((identifier) @variable.builtin (#set! priority 190))
-    ("." @lsp.type.function (#set! priority 190))
-  ]]
-)
+-- Implement using this.. weird trick?
+-- https://github.com/neovim/neovim/issues/41354
+local intim_patterns ---@type integer[]
+local toggle_counter = 0
 
-local do_color = nil
-local update = function(buf, tree)
-  if not do_color then return end
-  clear_all(buf)
-  for id, node, metadata, match in q:iter_captures(tree:root()) do
-    local srow, scol, erow, ecol = node:range()
-    vim.api.nvim_buf_set_extmark(buf, ns, srow, scol, {
-      end_row = erow,
-      end_col = ecol,
-      hl_group = "Error",
-      priority = 5000,
-    })
+---@alias Query vim.treesitter.Query
+---@type fun(_: lang):Query?
+local function get_query(lang)
+  local q = vim.treesitter.query.get(lang, "highlights")
+  if not q then
+    err.err("No query file found for " .. vim.inspect(lang) .. ".")
   end
-end
-
----@alias LanguageTree vim.treesitter.LanguageTree
----@return integer, LanguageTree?
-local function get_parser()
-  local buf = vim.api.nvim_get_current_buf()
-  local parser = vim.treesitter.get_parser(buf, lang)
-  if not parser then err.err("no parser found for " .. vim.inspect(lang)) end
-  return buf, parser
+  return q
 end
 
 local function start_coloring()
-  local buf, parser = get_parser()
-  if not parser then return end
-  local _, srow, scol, _ = unpack(vim.fn.getpos("w0"))
-  local _, erow, ecol, _ = unpack(vim.fn.getpos("w$"))
-  local range = { srow, scol, erow, ecol }
-  local tree = parser:tree_for_range(range)
-  if do_color == nil then -- First time: register callback.
-    parser:register_cbs(
-      { on_changedtree = function(_, tree) update(buf, tree) end },
-      true
-    )
+  local current = get_query(lang)
+  if not current then return end
+  local pats = current.info.patterns
+  local last_pattern = 0
+  for k, _ in pairs(pats) do
+    last_pattern = k
   end
-  do_color = true
-  update(buf, tree)
+  local q = [[;query
+    ;; extends
+    ((identifier) @variable.builtin (#set! priority 190))
+    ("." @lsp.type.function (#set! priority 190))
+  ]]
+  -- https://github.com/neovim/neovim/issues/41352
+  q = str.dedent(str.remove_prefix(";query\n", q))
+
+  -- https://github.com/neovim/neovim/issues/41354
+  if intim_patterns == nil then
+    local n_intim_patterns = #vim.treesitter.query.parse(lang, q).info.patterns
+    intim_patterns = {}
+    for i = 1, n_intim_patterns do
+      table.insert(intim_patterns, last_pattern + i)
+    end
+  else
+    q = q .. "\n;toggle-" .. toggle_counter
+    toggle_counter = toggle_counter + 1
+  end
+
+  vim.treesitter.query.set(lang, "highlights", q)
+  vim.treesitter.stop(0)
+  vim.treesitter.start(0, lang)
 end
 
 local function end_coloring()
-  local buf, parser = get_parser()
-  if not parser then return end
-  clear_all(buf)
-  do_color = false
+  local q = get_query(lang)
+  if not q then return end
+  for _, pat in ipairs(intim_patterns) do
+    q.query:disable_pattern(pat)
+  end
+  vim.treesitter.stop(0)
+  vim.treesitter.start(0, lang)
 end
 
 -- DEBUG.
